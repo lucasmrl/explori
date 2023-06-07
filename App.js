ed1bfc9ce069ea848fa0e046b5d37ef56fbad687
Lucas Lima <nutnewbr@gmail.com>
2023-06-06 22:32:25 -0500

    Before converting to Material Top Tabs Navigator

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TabView, TabBar, SceneMap } from "react-native-tab-view";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 function HomeScreen({ navigation }) {
   const FirstRoute = () => (
@@ -69,18 +70,29 @@ function HomeScreen({ navigation }) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.header_title}>Plano, TX</Text>
+      <View style={styles.container_view_r1}></View>
+      <View style={styles.container_view_r2}>
+        <Text style={styles.header_title}>Plano, TX</Text>
+      </View>
       <TabView
         renderTabBar={(props) => (
           <TabBar
             {...props}
             indicatorStyle={{
-              backgroundColor: "#A83C95",
-              marginLeft: 6,
-            }}
+              backgroundColor: "#A53891",
+            }} //469-600-1779
             // renderIndicator={() => <Text>!</Text>}
-            tabStyle={{ width: "auto", padding: 25 }}
-            activeColor="#A83C95"
+            // renderIcon={({ route, focused, color }) => (
+            //   <Ionicons
+            //     name="basketball"
+            //     size={focused ? 15 : 15}
+            //     color={color}
+            //   />
+            // )}
+            tabStyle={{
+              width: "auto",
+            }}
+            activeColor={"#A53891"}
             inactiveColor="#91959B"
             renderLabel={({ route, focused, color }) => (
               <Text
@@ -88,20 +100,17 @@ function HomeScreen({ navigation }) {
                   focused
                     ? {
                         color,
-                        padding: 0,
-                        fontSize: 16,
                         fontWeight: "bold",
-                        width: 200,
-                        height: 100,
+                        width: 80,
                       }
-                    : { color, margin: 0 }
+                    : { color, margin: 0, paddingBottom: 2 }
                 }
               >
                 {route.title}
               </Text>
             )}
             scrollEnabled={true}
-            style={{ backgroundColor: "white" }}
+            style={{ backgroundColor: "transparent" }}
           />
         )}
         navigationState={{ index, routes }}
@@ -109,6 +118,7 @@ function HomeScreen({ navigation }) {
         onIndexChange={setIndex}
         initialLayout={{ width: layout.width }}
       />
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -141,16 +151,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "",
   },
   container_view: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "",
     alignItems: "center",
     justifyContent: "center",
   },
+  container_view_r1: {
+    height: 44,
+    backgroundColor: "",
+  },
+  container_view_r2: {
+    height: 54,
+    display: "flex",
+    justifyContent: "center",
+  },
   header_title: {
-    paddingTop: 20,
     paddingLeft: 20,
     fontSize: 34,
     fontWeight: "bold",
